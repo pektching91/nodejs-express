df54dd0b89670e88dd97abfd3afae04bcf5a3398
pektching.chong@develab.io <pektching.chong@develab.io>
2021-09-28 11:20:40 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/hello', async (req, res) => {
+  return res.json({
+    hello: "world"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`)
+});
